3e304b49bd53fbebf4da48308e69b5bdc68a6601
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-18 12:55:52 +0000

    copy js and css files for tailwind

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -62,23 +62,16 @@ class InstallCommand extends Command
                 'sass-loader' => '^10.0.0',
             ] + $packages;
         });
-
-        // Webpack...
-        copy(__DIR__.'/../../stubs/resources/bootstrap/webpack.mix.js', base_path('webpack.mix.js'));
-
-        // Sass...
+        
         (new Filesystem)->ensureDirectoryExists(resource_path('sass'));
-
-        copy(__DIR__.'/../../stubs/resources/bootstrap/sass/_variables.scss', resource_path('sass/_variables.scss'));
-        copy(__DIR__.'/../../stubs/resources/bootstrap/sass/app.scss', resource_path('sass/app.scss'));
-
-        // Bootstrapping...
-        copy(__DIR__.'/../../stubs/resources/bootstrap/js/bootstrap.js', resource_path('js/bootstrap.js'));
-
-        // Files
         (new Filesystem)->ensureDirectoryExists(resource_path('views/partials'));
         (new Filesystem)->ensureDirectoryExists(resource_path('views/layouts'));
         
+        copy(__DIR__.'/../../stubs/resources/bootstrap/webpack.mix.js', base_path('webpack.mix.js'));
+        copy(__DIR__.'/../../stubs/resources/bootstrap/js/bootstrap.js', resource_path('js/bootstrap.js'));
+        copy(__DIR__.'/../../stubs/resources/bootstrap/sass/_variables.scss', resource_path('sass/_variables.scss'));
+        copy(__DIR__.'/../../stubs/resources/bootstrap/sass/app.scss', resource_path('sass/app.scss'));
+
         (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/boostrap/views/partials', resource_path('views/partials'));
         (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/boostrap/views/layouts', resource_path('views/layouts'));
     }
@@ -101,19 +94,19 @@ class InstallCommand extends Command
             ] + $packages;
         });
 
-        // Update Configuration...
         $this->replaceInFile('bootstrap', 'tailwind', config_path('trs-admin.php'));
-
-        // Webpack...
-        copy(__DIR__.'/../../stubs/resources/tailwind/webpack.mix.js', base_path('webpack.mix.js'));
-
-        // Tailwind config...
-        copy(__DIR__.'/../../stubs/resources/tailwind/tailwind.config.js', base_path('tailwind.config.js'));
-
-        // Files
+        
+        (new Filesystem)->ensureDirectoryExists(resource_path('js'));
+        (new Filesystem)->ensureDirectoryExists(resource_path('css'));
         (new Filesystem)->ensureDirectoryExists(resource_path('views/partials'));
         (new Filesystem)->ensureDirectoryExists(resource_path('views/layouts'));
         
+
+        copy(__DIR__.'/../../stubs/resources/tailwind/webpack.mix.js', base_path('webpack.mix.js'));
+        copy(__DIR__.'/../../stubs/resources/tailwind/tailwind.config.js', base_path('tailwind.config.js'));
+        copy(__DIR__.'/../../stubs/resources/tailwind/js/app.js', resource_path('js/app.js'));
+        copy(__DIR__.'/../../stubs/resources/tailwind/css/app.css', resource_path('css/app.css'));
+
         (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/tailwind/views/partials', resource_path('views/partials'));
         (new Filesystem)->copyDirectory(__DIR__.'/../../stubs/resources/tailwind/views/layouts', resource_path('views/layouts'));
     }
