c2b3aa672809f5cbf292cfb33e0971040632b952
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-18 16:41:57 +0000

    update form and form button style

--- resources/views/tailwind/components/button.blade.php
+++ resources/views/tailwind/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'py-3 mt-4 tracking-widest text-white text-center bg-yellow-500 hover:bg-yellow-400 focus:outline-none shadow-lg hover:shadow-none transition-all duration-300 ease-in-out']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'py-3 px-4 text-white text-center bg-yellow-500 font-semibold hover:bg-yellow-400 focus:outline-none shadow-lg hover:shadow-none transition-all duration-300 ease-in-out']) }}>
     {{ $slot }}
 </button>
--- resources/views/tailwind/components/form.blade.php
+++ resources/views/tailwind/components/form.blade.php
@@ -3,41 +3,43 @@
         <x-slot name="title">
             Hello there 👋, <span class="font-normal">please fill in your information to continue</span>
         </x-slot>
-    
+
         <x-slot name="content">
-            <form class="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-3" method="POST">
+            <form class="mt-6 grid grid-cols-1 sm:grid-cols-2 gap-4" method="POST">
                 @csrf
-    
+
                 <div class="col-span-1">
                     <x-trs-label for="firstname" value="Firstname" />
                     <x-trs-input id="firstname" type="text" name="firstname" placeholder="John" autocomplete="given-name" required />
                 </div>
-    
+
                 <div class="col-span-1">
                     <x-trs-label for="firstname" value="Lastname" />
                     <x-trs-input id="lastname" type="text" name="lastname" placeholder="Doe" autocomplete="family-name" required />
                 </div>
-    
+
                 <div class="col-span-1 sm:col-span-2">
                     <x-trs-label for="email" value="Email" />
                     <x-trs-input id="email" type="email" name="email" placeholder="john.doe@company.com" autocomplete="email" required />
                 </div>
-                
+
                 <div class="col-span-1 sm:col-span-2">
                     <x-trs-label for="password" value="Password" />
                     <x-trs-input id="password" type="password" name="password" placeholder="********" autocomplete="new-password" required />
                 </div>
-                
+
                 <div class="col-span-1 sm:col-span-2">
                     <x-trs-label for="password-confirm" value="Confirm Password" />
                     <x-trs-input id="password-confirm" type="password" name="password-confirm" placeholder="********" autocomplete="new-password" required />
                 </div>
-                
-                <x-trs-button>
-                    Sign Up
-                </x-trs-button>
-                
-                <a href="#" class="flex justify-between mt-4 text-xs text-gray-500 cursor-pointer hover:text-black transition-all duration-300 ease-in-out">Already registered?</a>
+
+                <div class="col-span-1 sm:col-span-2">
+                    <x-trs-button class="mt-2 w-full tracking-wider uppercase">
+                        Sign Up
+                    </x-trs-button>
+
+                    <a href="#" class="flex justify-between mt-4 text-xs text-gray-500 cursor-pointer hover:text-black transition-all duration-300 ease-in-out">Already registered?</a>
+                </div>
             </form>
         </x-slot>
     </x-trs-card>
