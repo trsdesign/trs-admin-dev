2a53b4ea5d73425a516b96219f7c76bae1476fef
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-18 17:14:41 +0000

    add all colors to tailwind config

--- stubs/resources/tailwind/tailwind.config.js
+++ stubs/resources/tailwind/tailwind.config.js
@@ -1,10 +1,37 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
     purge: ['./storage/framework/views/*.php', './resources/views/**/*.blade.php'],
 
     theme: {
         extend: {
+        },
+        colors: {
+            'blue-gray': colors.blueGray,
+            'cool-gray': colors.coolGray,
+            gray: colors.gray,
+            'true-gray': colors.trueGray,
+            'warm-gray': colors.warmGray,
+            red: colors.red,
+            amber: colors.amber,
+            orange: colors.orange,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            'light-blue': colors.lightBlue,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuschsia: colors.fuschsia,
+            pink: colors.pink,
+            rose: colors.rose,
+            black: colors.black,
+            white: colors.white,
         },
     },
 
