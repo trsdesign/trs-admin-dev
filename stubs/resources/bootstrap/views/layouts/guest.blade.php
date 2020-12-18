37b53e479661b90e382d0962b2ef1c8f084d6372
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-18 12:18:38 +0000

    comment out alpine

--- stubs/resources/bootstrap/views/layouts/guest.blade.php
+++ stubs/resources/bootstrap/views/layouts/guest.blade.php
@@ -14,7 +14,9 @@
         @stack('styles')
 
         <!-- Scripts -->
+        <!-- 
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
+        -->
 
     </head>
 
