d10520d790639a8d0c9b9045a2b129e8cb41d26b
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-18 16:26:50 +0000

    button style

--- resources/views/tailwind/components/button.blade.php
+++ resources/views/tailwind/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'w-full py-3 mt-6 font-medium tracking-widest text-white uppercase bg-black shadow-lg focus:outline-none hover:bg-yellow-500 hover:shadow-none transition-all duration-300 ease-in-out']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex py-3 mt-6 tracking-widest text-white text-center bg-black hover:bg-yellow-500 focus:outline-none shadow-lg hover:shadow-none transition-all duration-300 ease-in-out']) }}>
     {{ $slot }}
 </button>
