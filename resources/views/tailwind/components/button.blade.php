04b7c3b01f020dfbd48a012dcb85a1b2065890f5
Joel Butcher <joel@joelbutcher.co.uk>
2020-12-18 16:33:33 +0000

    more button style

--- resources/views/tailwind/components/button.blade.php
+++ resources/views/tailwind/components/button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex py-3 mt-6 tracking-widest text-white text-center bg-black hover:bg-yellow-500 focus:outline-none shadow-lg hover:shadow-none transition-all duration-300 ease-in-out']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'py-3 mt-4 tracking-widest text-white text-center bg-yellow-500 hover:bg-yellow-400 focus:outline-none shadow-lg hover:shadow-none transition-all duration-300 ease-in-out']) }}>
     {{ $slot }}
 </button>
--- resources/views/tailwind/components/subscribe-form.blade.php
+++ resources/views/tailwind/components/subscribe-form.blade.php
@@ -3,5 +3,7 @@
 
     <input type="email" class="flex-1 rounded-l border-gray-400" placeholder="Your email address" required>
 
-    <button class="flex-none inline-flex py-3 px-6 bg-yellow-500 hover:bg-black text-white font-semibold w-auto leading-6 transition-all duration-200 ease rounded-r border-none">Subscribe</button>
+    <x-trs-button class="flex-none inline-flex rounded-r border-none">
+        Subscribe
+    </x-trs-button>
 </form>
